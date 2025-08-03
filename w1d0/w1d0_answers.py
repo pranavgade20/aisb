a8c85ebe7d36506c8b127269ad51e0299055c44e
AISB Account <26707046+aisb-account@users.noreply.github.com>
2025-08-03 19:52:23 +0000

    Add git workflow test file

---
+++ w1d0/w1d0_answers.py
@@ -0,0 +1,22 @@
+
+# %%
+
+# Ensure the root directory is in the path for imports
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+from aisb_utils import report
+
+# Common imports
+import requests
+from typing import Callable
+
+print("It works!")
+
+#%%
+from w1d0_test import test_prerequisites
+
+
+# Run the prerequisite checks
+test_prerequisites()
