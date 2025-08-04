444855e15927367875f4ca9bfeccf6aca41f4aa7
AISB Account <26707046+aisb-account@users.noreply.github.com>
2025-08-04 13:11:08 +0100

    fixing container bug

---
+++ w1d1/w1d1_answers.py
@@ -0,0 +1,12 @@
+# %%
+
+import os
+import sys
+from typing import Generator, List, Tuple, Callable
+
+# Allow imports from parent directory
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
+from aisb_utils import report
+
+# %%
