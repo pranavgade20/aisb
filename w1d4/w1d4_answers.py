ee4a8034868aabf86008aa1801e1b73c9c3c67cb
AISB Account <26707046+aisb-account@users.noreply.github.com>
2025-08-07 10:36:32 +0000

    added file

--- w1d4/w1d4_answers.py
+++ w1d4/w1d4_answers.py
@@ -1,1 +1,21 @@
 # %%
+
+
+from typing import List
+import math
+from collections.abc import Callable
+import hmac
+import hashlib
+import os
+import sys
+from typing import Tuple, Optional, Callable, Literal
+import secrets
+import json
+from Crypto.Cipher import AES
+
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+from aisb_utils import report
+
+
+# %%
