f7807e6ef6f3cdf473c2c87009838c7fbb9f2fc1
AISB Account <26707046+aisb-account@users.noreply.github.com>
2025-08-14 14:39:51 +0000

    2b

--- w2d4/myproject/simpleServer.py
+++ w2d4/myproject/simpleServer.py
@@ -3,6 +3,6 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 import os
 
 os.chdir("/workspaces/aisb/w2d4/myproject/templates")
-server = HTTPServer(("0.0.0.0", 8000), SimpleHTTPRequestHandler)
-print("Server running on http://localhost:8000")
+server = HTTPServer(("0.0.0.0", 8001), SimpleHTTPRequestHandler)
+print("Server running on http://localhost:8001")
 server.serve_forever()
