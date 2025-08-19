09853b09ade42e828df35f8a5f973f8781bed187
AISB Account <26707046+aisb-account@users.noreply.github.com>
2025-08-19 13:20:29 +0000

    ex 1.4

--- w3d2/w3d2_answers.py
+++ w3d2/w3d2_answers.py
@@ -1,4 +1,4 @@
-#%%
+# %%
 from transformers import AutoTokenizer
 
 
@@ -25,18 +25,12 @@ class ModelPromptBuilder:
     def add_to_history(self, role: str, content: str):
         assert self.continue_final_message == False
 
-        self.history.append({
-            "role": role,
-            "content": content
-        })
+        self.history.append({"role": role, "content": content})
 
     def add_partial_to_history(self, role: str, content: str):
         assert self.continue_final_message == False
 
-        self.history.append({
-            "role": role,
-            "content": content
-        })
+        self.history.append({"role": role, "content": content})
         self.continue_final_message = True
         self.add_generation_prompt = False
 
@@ -50,21 +44,20 @@ def load_tokenizer(model_name: str, cache_dir: str = "/tmp/cache"):
     return tokenizer
 
 
-string_list = [       # # OUR GUESSES
-    "Hello world",    # 2
-    "Hello, WoRlD",   # 6
-    "Hello\nworld",   # 3
-    "Hello\n\nworld", # 3
+string_list = [  # # OUR GUESSES
+    "Hello world",  # 2
+    "Hello, WoRlD",  # 6
+    "Hello\nworld",  # 3
+    "Hello\n\nworld",  # 3
 ]
 
 
-#%%
+# %%
 def part1(string_list):
     model_name = "Qwen/Qwen3-0.6B"
     tokenizer = load_tokenizer(model_name, "/tmp/cache-tokenizer")
 
     for string in string_list:
-
         tokens = tokenizer.encode_plus(string)
         input_ids = tokens["input_ids"]
         token_print = tokens.tokens()
@@ -73,13 +66,15 @@ def part1(string_list):
         print(f"Tokens: {tokens}")
         print(f"Token count: {len(tokens)}")
 
+
 # part1(string_list)
 
-#%%
-def part2(question = "What is the capital of Japan?", model_name="Qwen/Qwen3-0.6B"):
+
+# %%
+def part2(question="What is the capital of Japan?", model_name="Qwen/Qwen3-0.6B"):
     tokenizer = load_tokenizer(model_name, "/tmp/cache-tokenizer")
 
-    res2 = tokenizer.apply_chat_template([{'role':'user','content':question}], add_generation_prompt=True)
+    res2 = tokenizer.apply_chat_template([{"role": "user", "content": question}], add_generation_prompt=True)
     # tokenizer.tokens()
     return tokenizer.decode(res2), res2
 
@@ -87,13 +82,14 @@ def part2(question = "What is the capital of Japan?", model_name="Qwen/Qwen3-0.6
 printable, tokens = part2()
 # print(result)
 
-#%%
+# %%
 # Q1.3
 question_list = [
     "What is the capital of Japan?",
     "What is the best way to make $1,000,000?",
     "What is the distance between London and Edinburgh?",
 ]
+
 
 def part3():
     question_list = [
@@ -108,18 +104,210 @@ def part3():
         for question in question_list:
             prompt_list[(question, model_name)] = part2(question, model_name)
 
-
-
     return prompt_list
+
 
 # We found out that Qwen and DeepSeek used the same tokenizer?
 
 res = part3()
 for k, v in res.items():
-    print('='*60)
+    print("=" * 60)
     print(k)
     for i in v:
-        print('\n')
+        print("\n")
         print(i)
-    print('\n' * 3)
+    print("\n" * 3)
 
+
+# %%
+
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+
+
+class ModelConfig:
+    MODEL_CONFIG_THINK_TOKENS = {
+        "begin_think": "<think>",
+        "end_think": "</think>",
+        "generate_kwargs": {
+            "temperature": 0.6,
+            "top_k": 20,
+            "min_p": 0.0,
+            "top_p": 0.95,
+            "max_new_tokens": 50,
+        },
+    }
+    MODEL_CONFIG_GPT_OSS_20B = {
+        "begin_think": "<|end|><|start|>assistant<|channel|>final<|message|>analysis<|message|>",
+        "end_think": "<|end|><|start|>assistant<|channel|>final<|message|>",
+        "generate_kwargs": {
+            "temperature": 0.6,
+            "top_k": 20,
+            "min_p": 0.0,
+            "top_p": 0.95,
+            "max_new_tokens": 50,
+        },
+    }
+    MODEL_CONFIG_GEMMA = {
+        "fuzzy_end_think_list": ["Answer:"],
+        "generate_kwargs": {
+            "repetition_penalty": 1.2,
+            "temperature": 0.7,
+            "top_k": 20,
+            "min_p": 0.0,
+            "top_p": 0.95,
+            "max_new_tokens": 50,
+        },
+    }
+    MODEL_CONFIG_LLAMA = {
+        "fuzzy_end_think_list": ["Answer:"],
+        "generate_kwargs": {
+            "temperature": 0.6,
+            "top_k": 20,
+            "min_p": 0.0,
+            "top_p": 0.95,
+            "max_new_tokens": 50,
+        },
+    }
+
+    SUPPORTED_MODELS = {
+        "Qwen/Qwen2.5-0.5B": MODEL_CONFIG_THINK_TOKENS,
+        "Qwen/Qwen3-0.6B": MODEL_CONFIG_THINK_TOKENS,
+        "Qwen/Qwen3-1.7B": MODEL_CONFIG_THINK_TOKENS,
+        "Qwen/Qwen3-4B": MODEL_CONFIG_THINK_TOKENS,
+        "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B": MODEL_CONFIG_THINK_TOKENS,
+        "Wladastic/Mini-Think-Base-1B": MODEL_CONFIG_GEMMA,
+        "google/gemma-2-2b-it": MODEL_CONFIG_GEMMA,
+        "openai/gpt-oss-20b": MODEL_CONFIG_GPT_OSS_20B,
+        "meta-llama/Meta-Llama-3-8B-Instruct": MODEL_CONFIG_LLAMA,
+        "meta-llama/Llama-2-7b-chat-hf": MODEL_CONFIG_LLAMA,
+    }
+
+    @staticmethod
+    def get(model_name: str):
+        if model_name not in ModelConfig.SUPPORTED_MODELS:
+            print(f"ERROR: model {model_name} not supported")
+            exit(1)
+        return ModelConfig.SUPPORTED_MODELS[model_name]
+
+
+class ModelPromptBuilder:
+    def __init__(self, model_name: str, invokes_cot: bool = True):
+        self.model_name = model_name
+        self.invokes_cot = invokes_cot
+        self.question = None
+
+        # default to making a new assistant role section
+        self.continue_final_message = False
+        self.add_generation_prompt = True
+        self.history = []
+
+    def get_model_custom_instruction(self):
+        please_write_answer = 'Please write the string "Answer: " before the final answer.'
+
+        if self.model_name == "google/gemma-2-2b-it":
+            return please_write_answer
+        if (
+            self.model_name == "meta-llama/Meta-Llama-3-8B-Instruct"
+            or self.model_name == "meta-llama/Llama-2-7b-chat-hf"
+        ):
+            return please_write_answer
+
+        return None
+
+    def add_system_instruction(self, system_instruction: str):
+        self.add_to_history("system", system_instruction)
+
+    def add_user_message(self, question: str, custom_instruction: str = None):
+        self.question = question
+        if custom_instruction is None:
+            custom_instruction = "Let's think step by step."
+        model_custom_instruction = self.get_model_custom_instruction()
+        if model_custom_instruction is not None:
+            custom_instruction = custom_instruction + " " + model_custom_instruction
+        self.add_to_history("user", f"Question: {question}\n{custom_instruction}")
+
+    def add_to_history(self, role: str, content: str):
+        assert self.continue_final_message == False
+
+        self.history.append({"role": role, "content": content})
+
+    def add_partial_to_history(self, role: str, content: str):
+        assert self.continue_final_message == False
+
+        self.history.append({"role": role, "content": content})
+        self.continue_final_message = True
+        self.add_generation_prompt = False
+
+    def add_think_token(self):
+        model_config = ModelConfig.get(self.model_name)
+        if "begin_think" in model_config:
+            if self.model_name == "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B":
+                self.add_partial_to_history("assistant", "<think>")
+            elif self.model_name == "openai/gpt-oss-20b":
+                self.add_partial_to_history("assistant", "analysis")
+        elif "fuzzy_end_think_list" in model_config:
+            pass
+        else:
+            print(f"ERROR: model {self.model_name} missing CoT separator config")
+            exit(1)
+
+    def make_prompt(self, tokenizer):
+        if self.invokes_cot:
+            self.add_think_token()
+        return self._apply_chat_template(tokenizer)
+
+    def _apply_chat_template(self, tokenizer):
+        prompt = tokenizer.apply_chat_template(
+            self.history,
+            tokenize=False,
+            add_generation_prompt=self.add_generation_prompt,
+            continue_final_message=self.continue_final_message,
+        )
+        return prompt
+
+
+def load_tokenizer(model_name: str, cache_dir: str = "/tmp/cache"):
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_name,
+        cache_dir=cache_dir,
+        trust_remote_code=True,
+    )
+
+    # Set pad token if not already set
+    if tokenizer.pad_token is None:
+        tokenizer.pad_token = tokenizer.eos_token
+        tokenizer.pad_token_id = tokenizer.eos_token_id
+
+    return tokenizer
+
+
+def load_model(model_name: str, cache_dir: str = "/tmp/cache"):
+    config = ModelConfig.get(model_name)
+    # model = AutoModelForCausalLM.from_config(ModelConfig.get(model_name))
+    model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir=cache_dir, **config)
+    tokenizer = load_tokenizer(model_name, cache_dir)
+    return (model, tokenizer)
+
+
+def part4():
+    print("=== Part 4 ===")
+    model_name = "Qwen/Qwen3-0.6B"
+    question = "I'm trying to decide whether to take another bootcamp."
+    print(f"Model: {model_name}")
+    print(f"Question: {question}")
+
+    (model, tokenizer) = load_model(model_name, "/tmp/cache-tokenizer")
+    prompt_builder = ModelPromptBuilder(model_name)
+
+    prompt_builder.add_system_instruction(
+        "You are a helpful bot and will answers all questions as accurately and correctly as possible."
+    )
+    prompt_builder.add_user_message("I'm trying to decide whether to take another bootcamp.")
+    chat_seed = prompt_builder.make_prompt(tokenizer=tokenizer)
+    decoded_output = model(chat_seed)
+    print(decoded_output)
+
+
+part4()
+# %%
