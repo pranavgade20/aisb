2adc8973fc00d67ed8eb385ea073e81acff81fca
Rusheb Shah <rusheb.shah@gmail.com>
2025-08-22 15:12:04 +0100

    Handshake, get_tools, get_resources

--- w3d5/w3d5_answers.py
+++ w3d5/w3d5_answers.py
@@ -1,22 +1,27 @@
 import json
 import threading
 import time
-from collections.abc import Iterable
 
 import requests
+from rich import print
 
 
 class UnknownEventError(RuntimeError):
     pass
 
 
-#
+JSONRPC = dict[str, str]
+Tool = dict[str, str]
+Resource = dict[str, str]
+
+
 class MCPClient:
     def __init__(self, url_base):
         self.url_base: str = url_base
         self.endpoint = None
         self.messages = []
         self.server_info = None
+        self.protocol_version = "2024-11-05"
 
     def connect(self):
         s = requests.Session()
@@ -27,22 +32,12 @@ class MCPClient:
         with s.get(sse_endpoint, headers=None, stream=True) as resp:
             for line in resp.iter_lines():
                 decoded_line: str = line.decode("utf-8")
+                print(decoded_line)
                 if decoded_line == "":
                     self._process_event(current_event)
                     current_event = []
                 else:
                     current_event.append(decoded_line)
-            # lines_iter: Iterable[bytes] = resp.iter_lines()
-            # for line in lines_iter:
-            # print("next")
-            # decoded: str = line.decode("utf-8")
-            # print(decoded)
-            # if "event: endpoint" in decoded:
-            #     data_line = next(lines_iter).decode("utf-8")
-            #     assert data_line.startswith("data: ")
-            #     session_endpoint = data_line.rstrip("data: ")
-            #     self.endpoint = session_endpoint
-            #     print(f"Endpoint = {self.endpoint}")
 
     def _process_event(self, event: list[str]):
         if len(event) == 2 and event[0] == ("event: endpoint") and event[1].startswith("data"):
@@ -54,14 +49,14 @@ class MCPClient:
             pass
         elif len(event) == 2 and event[0] == ("event: message") and event[1].startswith("data"):
             msg = event[1].removeprefix("data: ")
-            print(f"Found endpoint: {msg}")
+            print(f"Got msg: {msg}")
             json_data = json.loads(msg)
             self.messages.append(json_data)
         else:
             print(f"Unknown event: {event}")
             raise UnknownEventError(event)
 
-    def send_message(self, message):
+    def send_message(self, message: JSONRPC) -> requests.Response:
         """
         Send a message to the MCP server.
         :param message: The message to send.
@@ -69,30 +64,29 @@ class MCPClient:
         """
         if not self.endpoint:
             raise ValueError("Endpoint is not set. Connect to the MCP server first.")
-        # todo: send a message to the MCP server
-        print(message)
-        # eg "message": "tools/methods"
-        
-        headers = {'Accept': 'application/json, text/event-stream', 
-                   "Content-Type": "application/json"}
+
+        headers = {
+            "Accept": "application/json, text/event-stream",
+            "Content-Type": "application/json",
+            "MCP-Protocol-Version": self.protocol_version,
+        }
+        # TODO(rs) delete?
+        # if self.server_info:
+        #     headers["MCP-Protocol-Version"] = self.server_info["result"]["protocol_version"]
         json_rpc_msg = json.dumps(message)
 
         resp = requests.post(self.endpoint, data=json_rpc_msg, headers=headers)
-        assert resp.ok, f"expected OK got {resp.status_code}"
-        print(f"response: {resp.text}")
-        while len(self.messages) == 0:
-            time.sleep(0.5)
-        return self.messages.pop()
-
-        
+        assert resp.ok, f"expected OK got {resp.status_code}. Response: {resp.text}"
+        return resp
 
     def get_message(self):
         """
         Get the latest message from the MCP server.
         :return: The latest message received from the MCP server.
         """
-        # todo: wait for a message to show up in the queue and return it
-        pass
+        while len(self.messages) == 0:
+            time.sleep(0.5)
+        return self.messages.pop()
 
     def handshake(self):
         """
@@ -100,28 +94,64 @@ class MCPClient:
         """
         if not self.endpoint:
             raise ValueError("Endpoint is not set. Connect to the MCP server first.")
-        # todo: implement the handshake with the MCP server
-        #   - Send an initialization message to the MCP server with the protocol version and client info.
-        #   - Wait for the server's response and store it in self.server_info
-        pass
 
-    def get_resources(self, cursor=None):
+        initialize_request = {
+            "jsonrpc": "2.0",
+            "id": 1,
+            "method": "initialize",
+            "params": {
+                "protocolVersion": self.protocol_version,
+                "capabilities": {"roots": {"listChanged": True}, "sampling": {}, "elicitation": {}},
+                "clientInfo": {"name": "w3d5-diana-rusheb", "title": "Diana and Rusheb", "version": "1.0.0"},
+            },
+        }
+        self.send_message(initialize_request)
+        self.server_info = self.get_message()
+
+        print("Sending init")
+        initialized_notification = {
+            "jsonrpc": "2.0",
+            "method": "notifications/initialized",
+        }
+        resp = self.send_message(initialized_notification)
+        print(f"Init resp {resp}")
+        print("Handshake complete")
+
+    def get_resources(self, cursor=None) -> list[Resource]:
         """
         Get resources from the MCP server.
         :param cursor: Optional cursor for pagination.
         :return: List of resources.
         """
         # todo: implement the logic to list resources on the MCP server
-        pass
+        get_resourses_message = {"jsonrpc": "2.0", "id": 1, "method": "resources/list", "params": {}}
+        if cursor:
+            get_resourses_message["params"]["cursor"] = cursor
+        self.send_message(get_resourses_message)
+        resp = self.get_message()
+        tools = resp["result"]["resources"]
+        return tools
 
-    def get_tools(self, cursor=None):
+    def get_tools(self, cursor=None) -> list[Tool]:
         """
         Get tools from the MCP server.
         :param cursor: Optional cursor for pagination.
         :return: List of tools.
         """
         # todo: implement the logic to list tools on the MCP server
-        pass
+        get_tools_message = {
+            "jsonrpc": "2.0",
+            "id": "1",
+            "method": "tools/list",
+            "params": {},
+        }
+        if cursor:
+            get_tools_message["params"]["cursor"] = cursor
+
+        self.send_message(get_tools_message)
+        resp = self.get_message()
+        tools = resp["result"]["tools"]
+        return tools
 
     def access_resource(self, resource_uri):
         """
@@ -136,37 +166,19 @@ class MCPClient:
 if __name__ == "__main__":
     # Example usage
     mcp_client = MCPClient("https://0.mcp.aisb.dev")
-    
 
     print("started thread")
     thread = threading.Thread(target=mcp_client.connect).start()
     print("after thread")
     while not mcp_client.endpoint:
         time.sleep(0.1)
-        print("in the loop")
         print(".", end="")
     print(f"endpoint is: {mcp_client.endpoint}")
     mcp_client.handshake()
-    message = {
-        "jsonrpc": "2.0",
-        "id": 1,
-        "method": "initialize",
-        "params": {
-            "protocolVersion": "2024-11-05",
-            "capabilities": {
-            "roots": {
-                "listChanged": True
-            },
-            "sampling": {},
-            "elicitation": {}
-            },
-            "clientInfo": {
-            "name": "ExampleClient",
-            "title": "Example Client Display Name",
-            "version": "1.0.0"
-            }
-        }
-    }
-    print("about to send msg")
-    resp = mcp_client.send_message(message)
-    print(f"response: {resp}")
+
+    print("tools:", mcp_client.get_tools())
+    print("resources:", mcp_client.get_resources())
+
+
+## TODO
+# - request ID?
