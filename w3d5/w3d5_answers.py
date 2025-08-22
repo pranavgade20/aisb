44c0d75b440ff6d30249b028dfd4a8f522a779be
AISB Account <26707046+aisb-account@users.noreply.github.com>
2025-08-22 11:17:59 +0000

    ex 1.1

---
+++ w3d5/w3d5_answers.py
@@ -0,0 +1,21 @@
+# %%
+"""
+What are the main bottlenecks in the supply chain for AI accelerators?
+
+- TSMC's ability to manufacture chips (manufacturing output, maybe a little technology)
+- Non-destructive measurements
+
+How could an adversary exploit these bottlenecks?
+
+- On the manufacturing floot or in transit, anyone who puts hands on the chip can tamper with it
+
+What has been the effect of the export controls?
+
+- People just make it work with better technological work arounds
+
+Now that they have been lifted, what do you expect to see? Were the
+expert controls a good idea?
+
+- Export controls are bad long-term, but timelines might matter with such a quick technology cycle
+
+"""
